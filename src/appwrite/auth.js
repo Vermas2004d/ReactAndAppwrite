bab4e7571c28f20d43d84c833a9593d285d06c19
Vermas2004d <madhavverma971@gmail.com>
2025-09-16 11:48:08 +0530

    updating files

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -19,7 +19,6 @@ export class AuthService{
             if(userAccount){
                 // call another method
                 return this.login({email , password});
-
             }
             else {
                 return userAccount;
--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,31 @@
 import Header from "./Header/Header";
+import Select from "./Select"
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE.JSX";
+import SignUp from "./SignUp"
+import Login from "./Login"
+import Button from "./Button"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Input from "./Input"
 
 export {
     Header,
     Footer,
     Container,
-    Logo,
+    Logo, 
     LogoutBtn,
-    RTE
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    AuthLayout,
+    Input,
+    PostCard,
+    Button,
+    Select
 }
